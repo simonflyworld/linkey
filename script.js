b3e129088c6b60750144ffc129a414ea4a327f30
Ray Chang <raychang2017@gmail.com>
2020-03-13 23:35:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-class app {
+class App {
   constructor() {
     this.events();
   }
@@ -8,26 +8,22 @@ class app {
 		document.ondragstart = () => {return false;}
     document.oncontextmenu = () => {return false;}
     document.onload(() => {
-      this.mobileLockRotation();
+      this.mobileOrientationLock();
       this.jumpOverUrlBar();
     })
   }
 
-  mobileLockRotation() {
-    // If browser supports
-    if ('orientation' in screen) {
-      // document.documentElement.requestFullScreen();
-      screen.orientation.lock('portrait-primary'); // “portrait-primary”, “portrait-secondary”, “landscape-primary”, and “landscape-secondary”
+  mobileOrientationLock() {
+    screen.orientation.lock('portrait-primary'); // “portrait-primary”, “portrait-secondary”, “landscape-primary”, and “landscape-secondary”
       // Internet Explorer 11+
-      screen.msLockOrientation.lock("portrait-primary");
-      // Firefox
-      screen.mozLockOrientation.lock("portrait-primary");
-    
-      // screen.orientation.unlock();
-    }
+    screen.msLockOrientation.lock("portrait-primary");
+    // Firefox
+    screen.mozLockOrientation.lock("portrait-primary");
   }
 
   jumpOverUrlBar() {
     window.scrollTo(0, 1);
   }
 }
+
+let app = new App();
